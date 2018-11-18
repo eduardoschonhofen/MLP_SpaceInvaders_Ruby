6db72e176adc4cefd825b505e4f97b1ecc24c42f
Eduardo <eoschonhofen@inf.ufrgs.br>
2018-11-18 16:05:16 -0200

    Start

--- FunctionalGame.rb
+++ FunctionalGame.rb
@@ -1,17 +1,55 @@
+
+require('gosu')
+require_relative 'scripts/Definitions'
+Enemy=Struct.new(:posx,:posy,:alive)
+
 class FunctionalGame< Gosu::Window
 
   def initialize
     super Definitions::RES_WIDTH, Definitions::RES_HEIGHT,false
     self.caption= "Space Invaders MLP Funcional"
-    @enemys=array.new(30)
-    @blocks=array.new(20)
+    @totalEnemys=30
+    @totalBlocks=19
+
+
+    @enemys=startEnemysArray()
+   # @blocks=startBlocksArray()
+   # @player_shoot=startPlayerShoot()
+   # @enemy_shoot=startEnemyShoot()
+
     
 
 
   end
 
+  def startEnemysArray()
+    enemys=geraArray(nil,0)
+  end
+
+  def geraArray(,k)
+    if k>=@totalEnemys
+      return Array.new()
+    end
+    enemy=Enemy.new(0,0,true)
+    nextElement=geraArray(,k+1)
+    return nextElement.push(enemy)
+  end
+
+
+  def startBlocksArray()
+
+  end
+  def startPlayerShoot()
+
+  end
+  def startEnemyShoot()
+
+  end
+
+
 
   def update
+   # @enemys=updateEnemys(@enemys)
 
 
 
