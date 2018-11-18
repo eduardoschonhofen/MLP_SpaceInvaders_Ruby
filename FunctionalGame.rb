3421f649e5540772d5bc96b9114429a0cdb67e4c
Eduardo <eoschonhofen@inf.ufrgs.br>
2018-11-18 14:25:30 -0200

    Functional

---
+++ FunctionalGame.rb
@@ -0,0 +1,29 @@
+class FunctionalGame< Gosu::Window
+
+  def initialize
+    super Definitions::RES_WIDTH, Definitions::RES_HEIGHT,false
+    self.caption= "Space Invaders MLP Funcional"
+
+
+  end
+
+
+  def update
+
+
+
+
+
+  end
+
+
+
+  def draw
+
+
+  end
+
+end
+
+  window=FunctionalGame.new()
+  window.show
