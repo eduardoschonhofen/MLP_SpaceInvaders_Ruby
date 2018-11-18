b029019e6d1aaab5e9481f304c2a69e76de93b78
Eduardo <eoschonhofen@inf.ufrgs.br>
2018-11-18 20:05:14 -0200

    TAO DANDO TIRO DE NAVE

--- FunctionalGame.rb
+++ FunctionalGame.rb
@@ -6,10 +6,10 @@ require_relative 'scripts/Definitions'
 
 
 
-Enemy=Struct.new(:posx,:posy,:alive)
-Block=Struct.new(:posx,:posy,:alive)
+Enemy=Struct.new(:posx,:posy,:alive,:speed)
+Block=Struct.new(:posx,:posy,:alive,:speed)
 Shoot=Struct.new(:posx,:posy,:alive,:direction)
-Player=Struct.new(:posx,:posy,:alive)
+Player=Struct.new(:posx,:posy,:alive,:speed)
 
 class FunctionalGame< Gosu::Window
 
@@ -32,6 +32,7 @@ class FunctionalGame< Gosu::Window
     @player_shoot=startPlayerShoot()
     @enemy_shoot=startEnemyShoot()
     @player=startPlayer()
+    @direction=setRight
 
   end
 
@@ -52,7 +53,7 @@ class FunctionalGame< Gosu::Window
     if i==10
       return Array.new()
     end
-    enemy=Enemy.new((i+1)*60,(linha+1)*75-50,true)
+    enemy=Enemy.new((i+1)*60,(linha+1)*75-50,true,0.5)
     proximo=geraLinha(enemys[1..-1],i+1,linha)
     proximo.push(enemy)
   end
@@ -61,7 +62,7 @@ class FunctionalGame< Gosu::Window
     if k>=total
       return Array.new()
     end
-    enemy=tipo.new(0,0,true)
+    enemy=tipo.new(0,0,true,0)
     nextElement=geraArray(k+1,total,tipo)
     return nextElement.push(enemy)
   end
@@ -98,18 +99,130 @@ class FunctionalGame< Gosu::Window
   end
 
   def startPlayer()
-    return Player.new(Definitions::RES_WIDTH/2,Definitions::RES_HEIGHT-80,true)
+    return Player.new(Definitions::RES_WIDTH/2,Definitions::RES_HEIGHT-80,true,2)
   end
 
 
 
   def update
-   # @enemys=updateEnemys(@enemys)
+    @enemys=updateEnemys(@enemys)
+    playerAction=player_Controls(@player_shoot)
+
+    if playerAction != nil
+    @player=playerAction[0].call(@player)
+    @player_shoot=playerAction[1].call(@player_shoot,@player)
+    end
+  end
+
+  def updatePlayer(player)
+
+  end
+  def button_down(id)
+    if id==Gosu::KbEscape
+      close
+    end
+
+    return unless @tela_inicial
+    if id==Gosu::KbSpace
+      @tela_inicial = false
+    end
+  end
+
+  private
 
 
 
 
 
+  def player_Controls(player_shoot)
+    if button_down? Gosu::KbSpace and !player_shoot.alive
+      return [ident,newShoot]
+    end
+
+    if button_down? Gosu::KbLeft or button_down Gosu::GpLeft then
+      return [setLeft,ident2]
+    end
+    if button_down? Gosu::KbRight or button_down Gosu::GpRight then
+      return [setRight,ident2]
+    end
+  end
+
+  def ident
+    identidade = lambda do |obj|
+      return obj
+    end
+
+
+    return identidade
+  end
+
+  def ident2
+    identidade = lambda do |obj2,obj|
+      return obj2
+    end
+
+
+    return identidade
+  end
+  def newShoot
+    shoot = lambda do |obj2,obj|
+      if obj.alive and !obj2.alive
+        tiro=Shoot.new(obj.posx+15,obj.posy,true,-6)
+        return tiro
+      end
+    end
+
+    return shoot
+  end
+
+  def updateEnemys(enemys)
+    menorEsquerda=enemys.min_by(&:posx).posx
+    maiorDireita=enemys.max_by(&:posx).posx
+
+    if menorEsquerda<=0
+      @direction=setRight
+      enemys.map do
+      |enemy| enemy.posy=enemy.posy+10
+      end
+
+    end
+    if maiorDireita+@textureEnemy.width*0.025>=Definitions::RES_WIDTH
+      @direction=setLeft
+      enemys.map do
+        |enemy| enemy.posy=enemy.posy+10
+      end
+    end
+    enemys.each do
+      |enemy|
+      @direction.call(enemy)
+    end
+
+    return enemys
+
+
+
+  end
+
+  def setRight
+    moveRight = lambda do |obj|
+      if obj.alive
+        obj.posx=obj.posx+obj.speed
+        return obj
+      end
+    end
+
+    return moveRight
+  end
+
+  def setLeft
+    moveLeft = lambda do |obj|
+      if obj.alive
+        obj.posx=obj.posx-obj.speed
+        return obj
+      end
+    end
+
+    return moveLeft
   end
 
 
@@ -130,16 +243,11 @@ class FunctionalGame< Gosu::Window
     end
 
     @textureBackground.draw(1,1,0,1,1)
-
     drawScreen.call(@texturePlayer,@player,0,0.1,0.1)
 
+    @texturePlayerShoot.draw(@player_shoot.posx,@player_shoot.posy,1,0.1,0.1)
 
-
-
-
-
-
-
+    @player_shoot.posy=@player_shoot.posy+@player_shoot.direction
 
   end
 
